128c9303fac5e19fecfd02629725dfa7a7d4470f
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-11 17:42:22 +0530

    Commiting on UC2 branch with random function

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -7,6 +7,10 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+            int position = 0;
+            Random random = new Random();
+            int number = random.Next(0, 7);
+            Console.WriteLine(number);
         }
     }
 }
